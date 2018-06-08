1e9dc71cdab3a99752b9e624c507a994e29bd871
LoneWolf36 <runefaizan36@gmail.com>
2018-06-08 16:56:18 +0530

    Added custom react boilerplate

---
+++ Portfolio/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Comment {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to my portfolio</h1>
+            </div>
+        )
+    }
+}
+
+export default App
